9989a4b8779289c04d99acce85ad5fcb7d8c5a2c
Juliano Pereira Chagas <chagas.julianopereira@gmail.com>
2026-06-25 16:04:44 -0300

    Refactor Main class and implement banking operations

--- Main.java
+++ Main.java
@@ -1,13 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    public static void main(String[] args) {
+
+        ContaCorrente cc = new ContaCorrente();
+        cc.setTitular("Juliano");
+        cc.depositar(500);
+
+        System.out.println("Titular: " + cc.getTitular());
+        System.out.println("Saldo inicial: R$ " + cc.getSaldo());
+
+        cc.sacar(100);
+
+        System.out.println("Saldo após saque: R$ " + cc.getSaldo());
+
+        System.out.println();
+
+        ContaPoupanca cp = new ContaPoupanca();
+        cp.setTitular("Pedro");
+        cp.depositar(1000);
+
+        System.out.println("Titular: " + cp.getTitular());
+        System.out.println("Saldo inicial: R$ " + cp.getSaldo());
+
+        cp.renderJuros();
+
+        System.out.println("Saldo após juros: R$ " + cp.getSaldo());
     }
 }
